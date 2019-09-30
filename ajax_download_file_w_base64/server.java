267d3caafef8ff3dfb8f0683b29e5d2494ff25f9
Lam Du Thach <lamduthach@fabercompany.co.jp>
2019-09-30 16:07:12 +0900

    update java

--- ajax_download_file_w_base64/server.java
+++ ajax_download_file_w_base64/server.java
@@ -6,13 +6,21 @@
 public class Test extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        File file = new File("/path/to/test/file");
-        String fileBase64 = encodeFileToBase64(file);
-        file.delete();
-        PrintWriter out = response.getWriter();
-        out.print(fileBase64);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
+        PrintWriter out = null;
+        try {
+            File file = new File("/path/to/test/file");
+            String fileBase64 = encodeFileToBase64(file);
+            file.delete();
+            out = response.getWriter();
+            out.print(fileBase64);
+        } catch (IOException ex) {
+            // if something happen
+            out.print(new CustomException(ErrorType.ERROR_RUNNING, "Oops!!! Something happen for server"));
+        } finally {
+            out.close();
+        }
     }
 
     private String encodeFileToBase64(File originalFile) throws IOException {
@@ -23,6 +31,25 @@ public class Test extends HttpServlet {
             byte[] bytes = new byte[(int) originalFile.length()];
             fileInputStreamReader.read(bytes);
             return new String(Base64.encodeBase64(bytes));
+        }
+    }
+
+    public class CustomException extends Exception {
+
+        private final ErrorType errorType;
+        private final String message;
+
+        public CustomException(ErrorType errorType, String message) {
+            super(message);
+            this.errorType = errorType;
+            this.message = message;
+        }
+
+        public String getJsonString() {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorType", errorType.name());
+            jsonObject.addProperty("message", message);
+            return jsonObject.toString();
         }
     }
 }
